51aff8a17025b16a11c43ff960aac98c0e44b63f
rupeshvnair <rupeshviswanadhan@gmail.com>
2025-01-08 23:08:19 +0530

    Added Jan_8 leetcode

---
+++ leet_code/count_prefix_suffix_pairs.py
@@ -0,0 +1,49 @@
+'''
+You are given a 0-indexed string array words.
+
+Let's define a boolean function isPrefixAndSuffix that takes two strings, str1 and str2:
+
+isPrefixAndSuffix(str1, str2) returns true if str1 is both a
+prefix
+ and a
+suffix
+ of str2, and false otherwise.
+For example, isPrefixAndSuffix("aba", "ababa") is true because "aba" is a prefix of "ababa" and also a suffix, but isPrefixAndSuffix("abc", "abcd") is false.
+
+Return an integer denoting the number of index pairs (i, j) such that i < j, and isPrefixAndSuffix(words[i], words[j]) is true.
+
+Example 1:
+
+Input: words = ["a","aba","ababa","aa"]
+Output: 4
+Explanation: In this example, the counted index pairs are:
+i = 0 and j = 1 because isPrefixAndSuffix("a", "aba") is true.
+i = 0 and j = 2 because isPrefixAndSuffix("a", "ababa") is true.
+i = 0 and j = 3 because isPrefixAndSuffix("a", "aa") is true.
+i = 1 and j = 2 because isPrefixAndSuffix("aba", "ababa") is true.
+Therefore, the answer is 4.
+Example 2:
+
+Input: words = ["pa","papa","ma","mama"]
+Output: 2
+Explanation: In this example, the counted index pairs are:
+i = 0 and j = 1 because isPrefixAndSuffix("pa", "papa") is true.
+i = 2 and j = 3 because isPrefixAndSuffix("ma", "mama") is true.
+Therefore, the answer is 2.
+
+'''
+
+class Solution(object):
+    def countPrefixSuffixPairs(self, words):
+        i = 0
+        val = list()
+        while i < len(words):
+            value = [j for j in words[i+1:len(words)] if words[i]==j[0:len(words[i])] and words[i]==j[len(j)-len(words[i]):]]
+            val.extend(value)
+            i = i+1
+        return len(val)
+
+words = ["a","aba","ababa","aa"]
+nw_obj = Solution()
+val = nw_obj.countPrefixSuffixPairs(words)
+print(val)
